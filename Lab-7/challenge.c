e910fd576c7577e04d9a3f8feb8f96c06a475dd3
Chayaporn28 <chayapornnnnnn@gmail.com>
2025-09-18 15:32:02 +0000

    Complete Lab-7

---
+++ Lab-7/challenge.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    void swap(int *a,int *b) {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    void bubblesort(int *pi,int n) {
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - 1; j++) {
+                if(*(pi + j) > *(pi + j + 1)) {
+                    swap((pi + j),(pi + j + 1));
+                }
+            }
+        }
+    }
+    int num[5];
+    int *pt = num;
+    int size = 5;
+
+    printf("Enter 5 integers : ");
+    for(int i = 0; i < size; i++) {
+        scanf("%d",pt + i);
+    }
+    
+    bubblesort(pt,size);
+
+    printf("Sorted : ");
+    for(int i = 0; i < size; i++) {
+        printf("%d ",*(pt + i));
+    }
+    printf("\n");
+
+    return(0);
+}
